0f88707f3a94dd026dc5a1ce46dc485fc29d6961
malopez1578 <malopez1578@gmail.com>
2019-07-29 11:56:07 -0500

    Update: se agrega la funcion de cambiar el tamaño de fuente

--- filter.js
+++ filter.js
@@ -2,8 +2,9 @@
 // const btnSepia = document.querySelector('#theme-sepia');
 // const btnInvertir = document.querySelector('#theme-invertir');
 // const btnContrast = document.querySelector('#theme-contrast');
-const htmlElem = document.querySelector('html')
-const changeTheme = document.querySelector('#changeTheme')
+const htmlElem = document.querySelector('html');
+const changeTheme = document.querySelector('#changeTheme');
+const btnsChangeFontSize = [].slice.call(document.querySelectorAll('.changeSize'));
 
 // btnScaleGray.addEventListener('click', () => {
 //     htmlElem.style.setProperty('-webkit-filter', 'grayscale(1)')
@@ -18,11 +19,19 @@ const changeTheme = document.querySelector('#changeTheme')
 // btnContrast.addEventListener('click', () => {
 //     htmlElem.style.setProperty('filter', 'contrast(6)')
 // })
+function changeStyleFont(data) {
+    let fontSize = ['14px', '20px', '28px']
+    htmlElem.style.setProperty('--font_size', fontSize[data])
+}
+
+btnsChangeFontSize.map(elem => {
+    elem.addEventListener('click', e => {
+        dataFont = e.target.getAttribute('data');
+        changeStyleFont(dataFont);
+    })
+})
 
 changeTheme.addEventListener('change', (e) => {
     let valueTheme = e.target.value;
-
     htmlElem.setAttribute('hc', 'a'+valueTheme)
-    htmlElem.setAttribute('hcx', valueTheme)
-    
 })
